cb7f31b573e5524347e0573d121332ea259a5d3e
Yuhsak Inoue <yuhsak.inoue@gmail.com>
2022-12-02 17:03:08 +0900

    Update deps

--- jest.config.js
+++ jest.config.js
@@ -1,24 +1,10 @@
 module.exports = {
-  "roots": [
-    "<rootDir>/tests"
-  ],
-  "testMatch": [
-    "**/tests/**/*.+(ts|tsx|js|jsx)",
-    "**/?(*.)+(spec|test).+(ts|tsx|js|jsx)"
-  ],
-  "testPathIgnorePatterns": [
-    "/node_modules/",
-    "__.*?\.(ts|tsx|js|jsx)$"
-  ],
-  "collectCoverageFrom": [
-    "index.ts"
-  ],
-  "transform": {
-    "^.+\\.(ts|tsx)$": "ts-jest"
+  roots: ['<rootDir>/tests'],
+  testMatch: ['**/tests/**/*.+(ts|tsx|js|jsx)', '**/?(*.)+(spec|test).+(ts|tsx|js|jsx)'],
+  testPathIgnorePatterns: ['/node_modules/', '__.*?.(ts|tsx|js|jsx)$'],
+  collectCoverageFrom: ['index.ts'],
+  transform: {
+    '^.+\\.(ts|tsx)$': ['ts-jest', { tsconfig: 'tsconfig.test.json' }],
   },
-  "globals": {
-    "ts-jest": {
-      "tsConfig": "tsconfig.test.json"
-    }
-  }
+  testEnvironment: 'jest-environment-jsdom',
 }
